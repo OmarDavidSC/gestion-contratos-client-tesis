be801ccc3be06ac917e6534fe41225a26545103d
Jaxsito <jackabisai29@gmail.com>
2024-11-17 20:29:43 -0500

    cambios jackson

---
+++ app/src/app/components/profile/mi-perfil/recuperar-contraseña/recuperar-contraseña.componet.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-mi-perfil-recuperar-contraseña',
+    templateUrl: './recuperar-contraseña.componet.html',
+    styleUrls: ['./recuperar-contraseña.componet.scss']
+})
+
+export class RecuperarContraseñaComponent implements OnInit{
+
+    constructor(){}
+
+    ngOnInit(): void {
+    }
+    
+}
---
+++ app/src/app/components/profile/mi-perfil/reestablecer-contraseña/reestablecer-contraseña.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-mi-perfil-reestablecer-contraseña',
+    templateUrl: './reestablecer-contraseña.component.html',
+    styleUrls:['./reestablecer-contraseña.component.scss']
+})
+
+export class ReestablecerContraseñaComponet implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
